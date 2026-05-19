14eefb6a63975f6ecac933ef057510e72d61d3f3
alessandrodipalma00 <alessandrodipalma682@gmail.com>
2026-05-19 18:07:19 +0200

    Add files via upload

---
+++ CorsoWeb/Martedì_19/esempio.jhc/esempio.js
@@ -0,0 +1,8 @@
+function cambiaTesto(){
+    // seleziona l'elemento tramite id
+    let elemento = document.getElementById('testo');
+
+    // modifica il contenuto
+    elemento.textContent = 'Testo modificato!';
+    // come se avessi cambiato la proprietà di un oggetto
+}
