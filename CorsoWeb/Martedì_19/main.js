381d378c579c7d82a58571d3718beb841c4bbd0b
alessandrodipalma00 <alessandrodipalma682@gmail.com>
2026-05-19 16:23:13 +0200

    Add files via upload

---
+++ CorsoWeb/Martedì_19/main.js
@@ -0,0 +1,7 @@
+import {somma, PI} from './matematica.js';
+
+import moltiplica from './matematica.js';
+
+console.log(somma(2, 3));
+console.log(PI);
+console.log(moltiplica(2,3));
---
+++ CorsoWeb/Martedì_19/matematica.js
@@ -0,0 +1,12 @@
+
+export function somma(a,b) {
+    return a + b;
+}
+
+export const PI = 3.14;
+
+// default export
+
+export default function moltiplica(a,b) {
+    return a * b;
+}
