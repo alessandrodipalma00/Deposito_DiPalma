93f52cca37f344105e94bd05195ac5b76494e678
alessandrodipalma00 <alessandrodipalma682@gmail.com>
2026-05-20 17:50:16 +0200

    Add files via upload

---
+++ CorsoWeb/Mercoledì_20/manipolazione CSS/esercizio_promise.js
@@ -0,0 +1,40 @@
+let button = document.getElementById('btn');
+let output = document.getElementById('output');
+
+
+// definisco una promise che simula una richiesta asincrona
+function caricaDatiUtente(){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve({  // qui carico i dati dell'utente
+                nome : 'alessandro',
+                ruolo: 'studente',
+                stato: 'online'
+            });
+        },2000);
+    });
+}
+
+// ora utilizzo async/await
+async function caricaDati(){
+    output.textContent = 'Caricamento in corso...';
+
+    output.classList.remove('success');
+    output.classList.add('caricamento');
+
+    try{
+        let utente = await caricaDatiUtente();
+
+        output.innerHTML = 'Nome: ' + utente.nome + '<br>' +
+                           'Ruolo: ' + utente.ruolo + '<br>' + 
+                           'Stato: ' + utente.stato;
+
+        output.classList.remove('caricamento');
+        output.classList.add('success');
+
+    } catch( errore){
+        output.textContent = 'Errore nel caricamento dei dati';
+    }
+}
+// utilizzo del bottone per caricare i dati dell'utente
+button.addEventListener('click', caricaDati);
